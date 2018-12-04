bd97565895b1b317823e4f98d5bf814633781fcb
myapos <myapos@yahoo.com>
2018-12-05 00:55:51 +0200

    selected stellar starter

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/myros/Desktop/myProjects/whatismykino/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('/home/myros/Desktop/myProjects/whatismykino/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
