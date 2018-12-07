73aa00690704e79365a99e42b562567b8565be57
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2018-12-07 22:37:23 +0200

    displayed data in new site section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { graphql } from "gatsby";
 import { Link } from 'gatsby'
 import Helmet from 'react-helmet'
 import Waypoint from 'react-waypoint'
@@ -25,36 +26,45 @@ class Index extends React.Component {
   }
 
   render() {
+    // debugger;
+    // console.log(this);
 
-    return (
-      <Layout>
+
+    const { edges } = this.props.data.allNasaData;
+
+    console.log(edges);
+    return <Layout>
         <Helmet title="Gatsby Starter - Stellar" />
 
         <Header />
 
-        <Waypoint
-          onEnter={this._handleWaypointEnter}
-          onLeave={this._handleWaypointLeave}
-        >
-        </Waypoint>
+        <Waypoint onEnter={this._handleWaypointEnter} onLeave={this._handleWaypointLeave} />
         <Nav sticky={this.state.stickyNav} />
 
         <div id="main">
-
           <section id="intro" className="main">
             <div className="spotlight">
               <div className="content">
                 <header className="major">
                   <h2>Ipsum sed adipiscing</h2>
                 </header>
-                <p>Sed lorem ipsum dolor sit amet nullam consequat feugiat consequat magna
-                adipiscing magna etiam amet veroeros. Lorem ipsum dolor tempus sit cursus.
-                Tempus nisl et nullam lorem ipsum dolor sit amet aliquam.</p>
+                <p>
+                  Sed lorem ipsum dolor sit amet nullam consequat feugiat
+                  consequat magna adipiscing magna etiam amet veroeros.
+                  Lorem ipsum dolor tempus sit cursus. Tempus nisl et nullam
+                  lorem ipsum dolor sit amet aliquam.
+                </p>
                 <ul className="actions">
-                  <li><Link to="/generic" className="button">Learn More</Link></li>
+                  <li>
+                    <Link to="/generic" className="button">
+                      Learn More
+                    </Link>
+                  </li>
                 </ul>
               </div>
-              <span className="image"><img src={pic01} alt="" /></span>
+              <span className="image">
+                <img src={pic01} alt="" />
+              </span>
             </div>
           </section>
 
@@ -64,24 +74,37 @@ class Index extends React.Component {
             </header>
             <ul className="features">
               <li>
-                <span className="icon major style1 fa-code"></span>
+                <span className="icon major style1 fa-code" />
                 <h3>Ipsum consequat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <p>
+                  Sed lorem amet ipsum dolor et amet nullam consequat a
+                  feugiat consequat tempus veroeros sed consequat.
+                </p>
               </li>
               <li>
-                <span className="icon major style3 fa-copy"></span>
+                <span className="icon major style3 fa-copy" />
                 <h3>Amed sed feugiat</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <p>
+                  Sed lorem amet ipsum dolor et amet nullam consequat a
+                  feugiat consequat tempus veroeros sed consequat.
+                </p>
               </li>
               <li>
-                <span className="icon major style5 fa-diamond"></span>
+                <span className="icon major style5 fa-diamond" />
                 <h3>Dolor nullam</h3>
-                <p>Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat.</p>
+                <p>
+                  Sed lorem amet ipsum dolor et amet nullam consequat a
+                  feugiat consequat tempus veroeros sed consequat.
+                </p>
               </li>
             </ul>
             <footer className="major">
               <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
+                <li>
+                  <Link to="/generic" className="button">
+                    Learn More
+                  </Link>
+                </li>
               </ul>
             </footer>
           </section>
@@ -89,35 +112,54 @@ class Index extends React.Component {
           <section id="second" className="main special">
             <header className="major">
               <h2>Ipsum consequat</h2>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+              <p>
+                Donec imperdiet consequat consequat. Suspendisse feugiat congue
+                <br />
+                posuere. Nulla massa urna, fermentum eget quam aliquet.
+              </p>
             </header>
             <ul className="statistics">
               <li className="style1">
-                <span className="icon fa-code-fork"></span>
+                <span className="icon fa-code-fork" />
                 <strong>5,120</strong> Etiam
               </li>
               <li className="style2">
-                <span className="icon fa-folder-open-o"></span>
+                <span className="icon fa-folder-open-o" />
                 <strong>8,192</strong> Magna
               </li>
               <li className="style3">
-                <span className="icon fa-signal"></span>
+                <span className="icon fa-signal" />
                 <strong>2,048</strong> Tempus
               </li>
               <li className="style4">
-                <span className="icon fa-laptop"></span>
+                <span className="icon fa-laptop" />
                 <strong>4,096</strong> Aliquam
               </li>
               <li className="style5">
-                <span className="icon fa-diamond"></span>
+                <span className="icon fa-diamond" />
                 <strong>1,024</strong> Nullam
               </li>
             </ul>
-            <p className="content">Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum ac arcu sit amet, fermentum pellentesque et purus. Integer maximus varius lorem, sed convallis diam accumsan sed. Etiam porttitor placerat sapien, sed eleifend a enim pulvinar faucibus semper quis ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer eget purus nec nulla mattis et accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed ut magna ac risus et hendrerit scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras eu ornare dui curabitur lacinia.</p>
+            <p className="content">
+              Nam elementum nisl et mi a commodo porttitor. Morbi sit amet
+              nisl eu arcu faucibus hendrerit vel a risus. Nam a orci mi,
+              elementum ac arcu sit amet, fermentum pellentesque et purus.
+              Integer maximus varius lorem, sed convallis diam accumsan sed.
+              Etiam porttitor placerat sapien, sed eleifend a enim pulvinar
+              faucibus semper quis ut arcu. Ut non nisl a mollis est
+              efficitur vestibulum. Integer eget purus nec nulla mattis et
+              accumsan ut magna libero. Morbi auctor iaculis porttitor. Sed
+              ut magna ac risus et hendrerit scelerisque. Praesent eleifend
+              lacus in lectus aliquam porta. Cras eu ornare dui curabitur
+              lacinia.
+            </p>
             <footer className="major">
               <ul className="actions">
-                <li><Link to="/generic" className="button">Learn More</Link></li>
+                <li>
+                  <Link to="/generic" className="button">
+                    Learn More
+                  </Link>
+                </li>
               </ul>
             </footer>
           </section>
@@ -125,22 +167,74 @@ class Index extends React.Component {
           <section id="cta" className="main special">
             <header className="major">
               <h2>Congue imperdiet</h2>
-              <p>Donec imperdiet consequat consequat. Suspendisse feugiat congue<br />
-              posuere. Nulla massa urna, fermentum eget quam aliquet.</p>
+              <p>
+                Donec imperdiet consequat consequat. Suspendisse feugiat congue
+                <br />
+                posuere. Nulla massa urna, fermentum eget quam aliquet.
+              </p>
             </header>
             <footer className="major">
               <ul className="actions">
-                <li><Link to="/generic" className="button special">Get Started</Link></li>
-                <li><Link to="/generic" className="button">Learn More</Link></li>
+                <li>
+                  <Link to="/generic" className="button special">
+                    Get Started
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/generic" className="button">
+                    Learn More
+                  </Link>
+                </li>
               </ul>
             </footer>
           </section>
 
+          <section id="nasa" className="main special">
+            <header className="major">
+              <h2>Nasa data</h2>
+              <p>
+                Info
+                <br />
+                {edges.map(edge => <div>
+                  <p>Date: {edge.node.date}</p>
+                  <p>Title: {edge.node.title}</p>
+                  <p>Explanation: {edge.node.explanation}</p>
+                  <p><img src={edge.node.url}></img></p> 
+                  </div>)}
+              </p>
+            </header>
+            <footer className="major">
+              <ul className="actions">
+                <li>
+                  <Link to="/generic" className="button special">
+                    Get Started
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/generic" className="button">
+                    Learn More
+                  </Link>
+                </li>
+              </ul>
+            </footer>
+          </section>
         </div>
-
-      </Layout>
-    )
+      </Layout>;
   }
 }
 
+export const query = graphql`
+  query {
+    allNasaData {
+      edges {
+        node {
+          explanation
+          date
+          title
+          url
+        }
+      }
+    }
+  }
+`;
 export default Index
