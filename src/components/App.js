74cd64a22c2a38ca5ad776db91ff18c219dc1dd8
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2018-12-20 15:09:35 +0200

    linting

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import Content from "./Content";
-import Footer from "./Footer";
-import Nav from "./Nav";
-import Header from "./Header";
-import PropTypes from "prop-types";
-import '../css/css.styl'
+import Content from './Content';
+import Footer from './Footer';
+import Nav from './Nav';
+import Header from './Header';
+import PropTypes from 'prop-types';
+import '../css/css.styl';
 
-//https://api.opap.gr/draws/v3.0/1100/draw-date/2018-12-05/2018-12-05
+// https://api.opap.gr/draws/v3.0/1100/draw-date/2018-12-05/2018-12-05
 
 class App extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       ...props,
     };
   }
 
-  render() {
-
-    console.log('render:',this.state);
+  render () {
+    console.log('render:', this.state);
     return <div id="wrapper">
-        <Header />
-        <Nav />
-        {/* <Content /> */}
-        <Footer />
-      </div>;
+      <Header />
+      <Nav />
+      {/* <Content /> */}
+      <Footer />
+    </div>;
   }
 }
 
