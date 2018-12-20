446479f5a5c5a856dae258eb2169c0701082f997
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2018-12-20 23:40:36 +0200

    fixed scroll functionality for single page web app

--- src/components/presentational/Section.js
+++ src/components/presentational/Section.js
@@ -1,7 +1,31 @@
 import React from 'react';
 
+const scrollToTop = e => {
+  const goTo = document.getElementById('header');
+  // get dimensions
+
+  const dim = goTo.getBoundingClientRect();
+  // scroll to Top
+
+  window.scrollBy({
+    top: dim.top, // could be negative value
+    left: dim.left,
+    behavior: 'smooth',
+  });
+};
+
 const Section = props => (<div className="section">
-  { props.text }
+  <div className="subtitle" id={props.subtitle}>
+    {props.subtitle ? props.subtitle : 'subtitle' }
+  </div>
+  <div className="msg">
+    { props.text ? props.text : 'text' }
+    { props.children }
+  </div>
+  <div className="section_footer">
+    {props.section_footer ? props.section_footer : 'section footer'}
+    <span className="top" onClick={e => scrollToTop(e)}> Top </span>
+  </div>
 </div>);
 
 export default Section;
--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,52 @@
 export const headerTitle = 'headerTitle';
 
-export const intro = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+export const intro_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
 Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
 when an unknown printer took a galley of type and scrambled it to make a type 
 specimen book.It has survived not only five centuries, but also the leap into electronic 
 typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
 of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
 software like Aldus PageMaker including versions of Lorem Ipsum`;
+
+export const intro_subtitle = 'Introduction';
+export const intro_section_footer = 'Happy browsing';
+
+export const graphs_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type 
+specimen book.It has survived not only five centuries, but also the leap into electronic 
+typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
+of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+software like Aldus PageMaker including versions of Lorem Ipsum`;
+
+export const graphs_subtitle = 'Graphs';
+export const graphs_section_footer = 'graphs_section_footer';
+
+export const links_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type 
+specimen book.It has survived not only five centuries, but also the leap into electronic 
+typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
+of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+software like Aldus PageMaker including versions of Lorem Ipsum`;
+
+export const links_subtitle = 'Links';
+export const links_section_footer = 'links_section_footer';
+
+export const about_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type 
+specimen book.It has survived not only five centuries, but also the leap into electronic 
+typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
+of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
+software like Aldus PageMaker including versions of Lorem Ipsum`;
+
+export const about_subtitle = 'About';
+export const about_section_footer = 'about_section_footer';
+
+export const mapGoToElement = {
+  'Graphs': 'Graphs',
+  'Introduction': 'Introduction',
+  'Links': 'Links',
+  'About': 'About',
+};
