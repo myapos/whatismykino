a42964d86cf5fec853f2af5be4f80f9fd06d89ba
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2018-12-23 12:43:56 +0200

    get date picker in different file

--- src/components/container/Options.js
+++ src/components/container/Options.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import DateRangePicker from 'react-bootstrap-daterangepicker';
-import dayjs from 'dayjs';
-import moment from 'moment';
-// you will need the css that comes with bootstrap@3. if you are using
-// a tool like webpack, you can do the following:
-
-import 'bootstrap/dist/css/bootstrap.css';
-// you will also need the css that comes with bootstrap-daterangepicker
-import 'bootstrap-daterangepicker/daterangepicker.css';
 
 // import DatePicker from 'react-datepicker';
 import Select from 'react-select';
+import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { formatDate } from '../../utils/';
 
 import { fetchForDate } from '../../store/actions';
 
-// import 'react-datepicker/dist/react-datepicker.css';
-
-import PropTypes from 'prop-types';
+import MyCustomDatePicker from './MyCustomDatePicker';
 
 class Options extends Component {
   constructor (props) {
@@ -48,38 +38,12 @@ class Options extends Component {
   }
 
   handleEvent (event, picker) {
-    // debugger;
     console.log(picker.startDate);
   }
   render () {
-    // const { selectedDate, selectedOption } = this.state;
-    // const { kino } = this.props;
-
     return (
       <div id="options-container">
-        <DateRangePicker
-          startDate={moment().subtract(29, 'days')}
-          endDate={moment()}
-          onEvent={this.handleEvent} >
-          <div className="input-group" id="customizeInputGroup">
-            <input
-              type="text"
-              className="form-control"
-              readOnly
-              value={`test`} />
-            <span className="input-group-btn">
-              <button className="default date-range-toggle btn btn-default">
-                <div id="calImg" />
-              </button>
-            </span>
-          </div>
-        </DateRangePicker>
-        {/*  <DatePicker
-          className="dates"
-          selected={selectedDate}
-          placeholderText="Click to select a date"
-          onChange={this.handleDate}
-          dateFormat="yyyy-MM-dd" /> */}
+        <MyCustomDatePicker />
         {/* <Select
           className="select"
           value={selectedOption}
@@ -94,7 +58,18 @@ Options.propTypes = {
   fetchForDate: PropTypes.func,
 };
 
+// export default connect(
+//   state => ({ kino: state.kino }),
+//   { fetchForDate }
+// )(Options);
+
 export default connect(
-  state => ({ kino: state.kino }),
+  state => ({
+    startDate: state.startDate,
+    endDate: state.endDate,
+    maxDate: state.maxDate,
+  }),
   { fetchForDate }
 )(Options);
+
+// export default connect(state => state, actions)(Options);
