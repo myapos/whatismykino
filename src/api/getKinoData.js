c65f4de27b804aa3a83678fbe89f144f061aaae6
Apostolakis Myron <mapostolakis@phaistosnetworks.gr>
2018-12-25 12:05:39 +0200

    keep comments for graphql

--- src/api/getKinoData.js
+++ src/api/getKinoData.js
@@ -5,19 +5,18 @@ import resolver from '../utils/resolver';
 import initialQuery from '../queries/initialQuery';
 
 export default async () => {
+  console.log('initializations');
+  // keep this for later use with graphql
+  // const res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto/`).then(r => r.json());
 
-  console.log('getKinoData');
+  // console.log('res:', res);
+  // // debugger;
 
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto/`).then(r => r.json());
+  // console.log(initialQuery());
+  // // Filter the data!
+  // const result = graphql(resolver, initialQuery(), res);
 
-  console.log('res:', res);
-  // debugger;
+  // console.log(result);
 
-  console.log(initialQuery());
-  // Filter the data!
-  const result = graphql(resolver, initialQuery(), res);
-
-  console.log(result);
-
-  return result;
-  };
+  return true;
+};
