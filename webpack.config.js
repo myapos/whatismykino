bc33b0203b84932dc4b3ee9cb2dc3fdc4dbe7073
myapos <myapos@yahoo.com>
2019-01-06 23:42:00 +0200

    minor

--- webpack.config.js
+++ webpack.config.js
@@ -7,79 +7,80 @@ const path = require("path");
 // const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 // const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
 
+
 const PORT = 1234;
 
 const config = {
-  entry: {
-    main: "./src/index.js"
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: `http://localhost:${PORT}/dist/`
-  },
-  resolve: {
-    alias: {
-      react: path.resolve(__dirname, "node_modules", "react"),
-      "react-dom": path.resolve(__dirname, "node_modules", "react-dom")
-    }
-  },
-  node: {
-    fs: "empty",
-    child_process: "empty"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: "babel-loader",
-        include: [path.resolve(__dirname, "./src")]
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        use: "file-loader"
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      },
-      {
-        test: /\.styl$/,
-        // use: 'css-loader',
-        use: [
-          {
-            loader: "style-loader" // creates style nodes from JS strings
-          },
-          {
-            loader: "css-loader" // translates CSS into CommonJS
-          },
-          {
-            loader: "stylus-loader" // compiles Stylus to CSS
-          }
+    entry: {
+        main: "./src/index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        publicPath: `http://localhost:${PORT}/dist/`
+    },
+    resolve: {
+        alias: {
+            react: path.resolve(__dirname, "node_modules", "react"),
+            "react-dom": path.resolve(__dirname, "node_modules", "react-dom")
+        }
+    },
+    node: {
+        fs: "empty",
+        child_process: "empty"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader",
+                include: [path.resolve(__dirname, "./src")]
+            },
+            {
+                test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+                use: "file-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+            },
+            {
+                test: /\.styl$/,
+                // use: 'css-loader',
+                use: [
+                    {
+                        loader: "style-loader" // creates style nodes from JS strings
+                    },
+                    {
+                        loader: "css-loader" // translates CSS into CommonJS
+                    },
+                    {
+                        loader: "stylus-loader" // compiles Stylus to CSS
+                    }
+                ]
+            }
         ]
-      }
+    },
+    devServer: {
+        hot: true,
+        overlay: true,
+        noInfo: false,
+        compress: true,
+        port: PORT,
+        publicPath: `http://localhost:${PORT}/dist/`,
+        disableHostCheck: true,
+        headers: { "Access-Control-Allow-Origin": "*" }
+    },
+    plugins: [
+        new webpack.BannerPlugin({
+            banner: `Build: ${new Date().toLocaleString()}`
+        }),
+        new webpack.DefinePlugin({
+            "process.env": {
+                USAGE: JSON.stringify(process.env.USAGE)
+            }
+        })
     ]
-  },
-  devServer: {
-    hot: true,
-    overlay: true,
-    noInfo: false,
-    compress: true,
-    port: PORT,
-    publicPath: `http://localhost:${PORT}/dist/`,
-    disableHostCheck: true,
-    headers: { "Access-Control-Allow-Origin": "*" }
-  },
-  plugins: [
-    new webpack.BannerPlugin({
-      banner: `Build: ${new Date().toLocaleString()}`
-    }),
-    new webpack.DefinePlugin({
-      "process.env": {
-        USAGE: JSON.stringify(process.env.USAGE)
-      }
-    })
-  ]
 };
 
 module.exports = config;
