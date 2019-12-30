caa417e086941cd95519c3bd64b1c5846f76d85e
Myron Apostolakis <myron.apostolakis@sunlight.io>
2019-12-30 22:56:05 +0200

    change dummy text

--- client/constants.js
+++ client/constants.js
@@ -17,33 +17,20 @@ export const intro_section_footer = "Happy browsing";
 
 export const graphs_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
 Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type 
-specimen book.It has survived not only five centuries, but also the leap into electronic 
-typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
-of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-software like Aldus PageMaker including versions of Lorem Ipsum`;
+when an unknown printer took a galley of type and scrambled it to make a type`;
 
 export const graphs_subtitle = "Graphs";
 export const graphs_section_footer = "graphs_section_footer";
 
 export const links_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
 Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type 
-specimen book.It has survived not only five centuries, but also the leap into electronic 
-typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
-of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-software like Aldus PageMaker including versions of Lorem Ipsum`;
+when an unknown printer took a galley of type and scrambled it to make a type `;
 
 export const links_subtitle = "Links";
 export const links_section_footer = "links_section_footer";
 
 export const about_msg = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, 
-when an unknown printer took a galley of type and scrambled it to make a type 
-specimen book.It has survived not only five centuries, but also the leap into electronic 
-typesetting, remaining essentially unchanged.It was popularised in the 1960s with the release 
-of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-software like Aldus PageMaker including versions of Lorem Ipsum`;
+Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, `;
 
 export const about_subtitle = "About";
 export const about_section_footer = "about_section_footer";
